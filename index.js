c3ab538932b8c81cc01ceba8e7d57f60d2b6a212
Joseph Sutton <josesutt@cisco.com>
2015-10-24 21:58:29 -0700

    fixed path name for legacy version (ES5)

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var isES6 = require('./lib/util.js').isES6;
 if ( isES6() ) {
   module.exports = require('./lib/hyper.js');
 } else {
-  module.exports = require('./es5/hyper.js');
+  module.exports = require('./legacy/hyper.js');
 }
